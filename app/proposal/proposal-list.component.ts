8a82b6f0debbac5015eb74cf52f6e95de271636d
kinkorea <alexkarasik89@gmail.com>
2017-08-17 16:39:06 -0400

    set up bootstrap and set a background color

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -2,20 +2,18 @@ import { Component } from '@angular/core';
 import { Proposal } from './proposal';
 
 @Component({
-  moduleId: module.id,
-  selector: 'proposal-list',
-  templateUrl: 'proposal-list.component.html'
+	moduleId: module.id,
+	selector: 'proposal-list',
+	templateUrl: 'proposal-list.component.html'
 })
-
 export class ProposalListComponent {
-  proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://alexkarasik.com', 'Ruby on Rails', 150, 120, 15, 'alexkarasik89@gmail.com')
-  proposalTwo: Proposal = new Proposal(99, 'Xyz Company', 'http://alexkarasik.com', 'Ruby on Rails', 150, 120, 15, 'alexkarasik89@gmail.com')
-  proposalThree: Proposal = new Proposal(100, 'Something Company', 'http://alexkarasik.com', 'Ruby on Rails', 150, 120, 15, 'alexkarasik89@gmail.com')
+	proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
+	proposalTwo: Proposal = new Proposal(99, 'XYZ Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
+	proposalThree: Proposal = new Proposal(300, 'Something Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
 
-
-  proposals: Proposal[] = [
-    this.proposalOne,
-    this.proposalTwo,
-    this.proposalThree
-  ]
+	proposals: Proposal[] = [
+		this.proposalOne,
+		this.proposalTwo,
+		this.proposalThree
+	]
 }
